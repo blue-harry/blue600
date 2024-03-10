0858d7681dc45f5084c945ff78b13331aec7bde7
Bihai <nutapp@foxmail.com>
2024-03-10 16:13:14 +0800

    update miniplay

--- pages/links.vue
+++ pages/links.vue
@@ -1,17 +1,22 @@
 <template>
     <div class="d-flex justify-space-around">
-      <div class="my-4 d-flex flex-column align-center">
-        <v-img :width="200" :aspect-ratio="1" src="/public/crazygames.jpeg" alt="FlappyBird" cover></v-img>
-        
-        <NuxtLink to="https://www.crazygames.com/" target="_blank">
+        <div class="my-4 d-flex flex-column align-center">
+            <v-img :width="200" :aspect-ratio="1" src="/public/crazygames.jpeg" alt="Crazy games" cover></v-img>
+
+            <NuxtLink to="https://www.crazygames.com/" target="_blank">
                 Crazy games
             </NuxtLink>
-      </div>
+        </div>
+        <div class="my-4 d-flex flex-column align-center">
+            <v-img :width="200" :aspect-ratio="1" src="/public/miniplay.jpeg" alt="Miniplay" cover></v-img>
 
-      
+            <NuxtLink to="https://www.miniplay.com/mario-bros-games" target="_blank">
+                Miniplay
+            </NuxtLink>
+        </div>
     </div>
 
-            
+
 
 
 </template>
