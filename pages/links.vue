247e860a1e553529bb88b84f4c47ec31f244bede
Bihai <nutapp@foxmail.com>
2024-03-11 17:12:41 +0800

    edit by Jason

--- pages/links.vue
+++ pages/links.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- <v-container class="d-none d-sm-flex flex-column">
+    <!-- <v-container>
         <div class="d-flex justify-space-around">
             <div class="my-4 d-flex flex-column align-center">
                 <v-img :width="200" :aspect-ratio="1" src="/public/crazygames.jpeg" alt="Crazy games" cover></v-img>
@@ -22,12 +22,13 @@
     <!-- 电脑版 -->
     <v-container class="d-none d-sm-flex flex-column">
         <div class="text-center text-h4 mb-6">
-            This is my cloud games!
+            These are my favourite websites!
         </div>
 
         <div class="d-flex justify-space-around">
             <div class="my-4 d-flex flex-column align-center">
                 <v-img :width="200" :aspect-ratio="1" src="/public/crazygames.jpeg" alt="Crazy games" cover></v-img>
+
                 <NuxtLink to="https://www.crazygames.com/" target="_blank">
                     Crazy games
                 </NuxtLink>
@@ -35,22 +36,24 @@
 
             <div class="my-4 d-flex flex-column align-center">
                 <v-img :width="200" :aspect-ratio="1" src="/public/miniplay.jpeg" alt="Miniplay" cover></v-img>
+
                 <NuxtLink to="https://www.miniplay.com/mario-bros-games" target="_blank">
                     Miniplay
                 </NuxtLink>
             </div>
         </div>
     </v-container>
-
+    
     <!-- 手机版 -->
     <v-container class="d-sm-none">
         <div class="text-center text-h5 text-sm-h3 mb-8">
-            This is my cloud games!
+            These are my favourite websites!
         </div>
 
         <div class="d-sm-flex justify-space-around">
             <div class="my-4 d-flex flex-column align-center">
                 <v-img :width="200" :aspect-ratio="1" src="/public/crazygames.jpeg" alt="Crazy games" cover></v-img>
+
                 <NuxtLink to="https://www.crazygames.com/" target="_blank">
                     Crazy games
                 </NuxtLink>
@@ -58,12 +61,11 @@
 
             <div class="my-4 d-flex flex-column align-center">
                 <v-img :width="200" :aspect-ratio="1" src="/public/miniplay.jpeg" alt="Miniplay" cover></v-img>
+
                 <NuxtLink to="https://www.miniplay.com/mario-bros-games" target="_blank">
                     Miniplay
                 </NuxtLink>
             </div>
         </div>
     </v-container>
-
-
 </template>
